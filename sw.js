00cdf382651cf4060f436f840e3296a8876913b6
albubueno-eng <albu.bueno@gmail.com>
2026-04-30 15:12:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //  MOVIFIT — Service Worker v1.0
 // ══════════════════════════════════════════════════════════════
 
-var CACHE_NAME = 'movifit-v1.9';
+var CACHE_NAME = 'movifit-v2.0';
 var ASSETS = [
   './',
   './index.html',
